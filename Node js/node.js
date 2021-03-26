841de41a68e0931eed4483736996fb40a934fce1
unknown <heyitskeagan@gmail.com>
2021-03-26 13:46:50 -0700

    Adding Test Site to JS File

---
+++ Node js/node.js
@@ -0,0 +1,27 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    if(filename == './') {filename = './index';}
+
+    filename = filename + ".html";
+
+    fs.readFile(filename, function(err, data){
+        if (err) {
+            res.writeHead(404, {'Conent-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Conent-Type': 'text/html'});
+        res.write(data);
+        console.log("...Incomming request "+ req.url);
+        return res.end();
+    });
+}).listen(8080);
+
+console.log("Server Listening On port 8080...");
+
+
